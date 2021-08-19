121dc52bb87668350c45f9ead9e906b14b8a2624
adimidania <ja_adimi@esi.dz>
2021-08-19 23:46:59 +0200

    tablet + mobile

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector(".share-icon").addEventListener("click", function () {
+    document.querySelector(".active").classList.toggle("visible");
+    document.querySelector(".share-icon").classList.toggle("change-button");
+  });
